a60aa0df1aaab6c2b43a330f09d1d8da90b4beb0
Kurmaya Siddhartha <kurmaya.sid@gmail.com>
2024-02-07 21:17:43 +0530

    updated win conditions and animation ease

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ const iconWidth = 79,
 const roll = (reel , offset = 0) => {
 const delta = (offset + 2 )* numIcons + Math.round(Math.random()* numIcons) ;
 const style = getComputedStyle(reel),
-backgroundPositionY = parseFloat(style['backgroundPositionY']);
+backgroundPositionY = parseFloat(style['backgroundPositionY']),
+targetBackgroundPositionY = backgroundPositionY + delta * iconHeight,
+normalTargetBackgroundPositionY = targetBackgroundPositionY%(numIcons * iconHeight);
+
 return new Promise((resolve , reject)=> {
 
-    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms`;
-    reel.style.backgroundPositionY = `${backgroundPositionY + delta *iconHeight}px`;
+    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms cubic-bezier(.45,.05,.58,1.09)`;
+    reel.style.backgroundPositionY = `${targetBackgroundPositionY}px`;
+    
+
+
     setTimeout(() => {
+        reel.style.transition=`none`;
+        reel.style.backgroundPositionY = `${normalTargetBackgroundPositionY}px`;
         resolve(delta%numIcons)
     }, 8 + delta * time_per_icon)
 })
@@ -27,12 +35,18 @@ function rollAll() {
         
     // },4000);
     Promise
-    .all([...reelsList].map((reel,i) => roll(reel,i)))
+    .all( [...reelsList].map((reel,i) => roll(reel,i)) )
     .then((deltas)=> {
         deltas.forEach((delta,i)=>indexes[i] =(indexes[i] + delta)%numIcons);
         indexes.map((index) => {console.log(nameIndex[index],indexes)})
         document.querySelector('.roll').disabled=false;
+        if (indexes[0] == indexes[1] || indexes[1] == indexes[2]){
+            console.log('WIN WIN WIN');
+            alert('WIN WIN WIN!');
+        }
     })
+    //check win conditions
+
     // [...reelsList].map((reel , i )=> {
 
     //     console.log(reel,i);
